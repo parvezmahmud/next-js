5913ca972e500cab869d7edf9e970add134e6739
mahmud parvez <mahmudparvez05@gmail.com>
2021-02-22 20:41:24 +0600

    changed id to popularUID

--- components/populars/popular.data.js
+++ components/populars/popular.data.js
@@ -1,27 +1,20 @@
 export default [
     {
-        id:"popular-for-a1",
+        popularUID:"popular-for-a1",
         title:"Crimson Cup",
         description:"Offers the best coffee in town",
         classes:"bg-purple-300 lg:bg-opacity-0 hover:bg-opacity-80 h-20 w-full bg-opacity-80 transition ease-in duration-300",
         image:"https://images.unsplash.com/photo-1552566626-52f8b828add9?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
     },
     {
-        id:"popular-for-a2",
+        popularUID:"popular-for-a2",
         title:"Gloria Jeans",
         description:"Home feeling",
         classes:"bg-indigo-400 lg:bg-opacity-0 hover:bg-opacity-80 h-20 w-full bg-opacity-80 transition ease-in duration-300",
         image:"https://images.unsplash.com/photo-1500898335608-a4b6696f81a5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
     },
     {
-        id:"popular-for-a3",
-        title:"North End",
-        description:"Home Brewed and Premium Quality",
-        classes:"bg-purple-400 lg:bg-opacity-0 hover:bg-opacity-80 h-20 w-full bg-opacity-80 transition ease-in duration-300",
-        image:"https://images.unsplash.com/photo-1484659619207-9165d119dafe?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=750&q=80"
-    },
-    {
-        id:"popular-for-a3",
+        popularUID:"popular-for-a3",
         title:"North End",
         description:"Home Brewed and Premium Quality",
         classes:"bg-purple-400 lg:bg-opacity-0 hover:bg-opacity-80 h-20 w-full bg-opacity-80 transition ease-in duration-300",

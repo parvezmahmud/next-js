4ff45cae667e5a4a870fa3462b1d0c2d71c6b390
mahmud parvez <mahmudparvez05@gmail.com>
2021-02-28 22:06:43 +0600

    style modified

--- components/featured/featured.components.js
+++ components/featured/featured.components.js
@@ -1,4 +1,5 @@
 import FeaturedData from './featured.data';
+import Link from 'next/link';
 
 const FeaturedRestaurants = () => {
     return(
@@ -8,18 +9,24 @@ const FeaturedRestaurants = () => {
             </div>
             <div className="grid grid-cols-3 p-4 text-center text-white space-x-2">
             
-                {FeaturedData.slice(0,3).map((featured) => (
-                    <div className="shadow-xl res-box h-60 rounded-md" style={{backgroundImage:`url(${featured.image})`}}>
-                        <div className={`${featured.classes}`}>
-                            <h2 className="text-md sm:text-2xl">{featured.title}</h2>
-                            <p className="text-xs md:text-sm">{featured.description}</p>
-                        </div>
+                {FeaturedData.slice(0,3).map((item, index) => (
+                    <div key={index}>
+                        <Link as={`/all-featured/${item.featuredUID}/${item.title}`} href="/all-featured/[featuredUID]/[title]">
+                            <div className="shadow-xl res-box h-60 rounded-md" style={{backgroundImage:`url(${item.image})`}}>
+                                <div className={`${item.classes}`}>
+                                    <h2 className="text-md sm:text-2xl">{item.title}</h2>
+                                    <p className="text-xs md:text-sm">{item.description}</p>
+                                </div>
+                            </div>
+                        </Link>
                     </div>
                 ))}
 
         </div>
         <div className="text-center pt-1">
-            <button className="border-solid border-4 h-10 w-20 bg-gray-100 transform scale-100 hover:scale-125 rounded text-md tracking-wider shadow-xl opacity-100 transition ease-in duration-700">more</button>
+            <Link href="/featured">
+                <button className="h-10 w-20 bg-white transform scale-100 hover:scale-125 rounded text-md tracking-wider shadow-xl opacity-100 transition ease-in duration-700">more</button>
+            </Link>
         </div>
 
         <style>

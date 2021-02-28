7dea37c783e93f44f230e5b23e9a40caa4dea5b3
mahmud parvez <mahmudparvez05@gmail.com>
2021-02-28 22:07:12 +0600

    style modified

--- components/featured/featured.data.js
+++ components/featured/featured.data.js
@@ -1,17 +1,20 @@
 export default [
     {
+        featuredUID:"ashvgw",
         title:"Featured item one",
         description:"This is a great place to spend time with your friends and family",
         classes:"bg-gray-900 lg:bg-opacity-0 hover:bg-opacity-80 h-20 w-full bg-opacity-80 transition ease-in duration-300",
         image:"https://images.unsplash.com/photo-1514933651103-005eec06c04b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=667&q=80",
     },
     {
+        featuredUID:"asdff",
         title:"Featured item two",
         description:"This is a great place to spend time with your friends and family",
         classes:"bg-red-300 lg:bg-opacity-0 hover:bg-opacity-80 h-20 w-full bg-opacity-80  transition ease-in duration-300",
         image:"https://images.unsplash.com/photo-1587574293340-e0011c4e8ecf?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjB8fHJlc3RhdXJhbnR8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
     },
     {
+        featuredUID:"aaw33",
         title:"Featured item three",
         description:"This is a great place to spend time with your friends and family",
         classes:"bg-green-300 lg:bg-opacity-0 hover:bg-opacity-80 h-20 w-full bg-opacity-80  transition ease-in duration-300",
